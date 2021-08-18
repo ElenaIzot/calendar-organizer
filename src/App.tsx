0ebee00262784b0f14fbc2824b98f2484c73fece
Elena Izotova <elm57650@gmail.com>
2021-08-18 10:26:24 +0700

    added gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import 'react-calendar/dist/Calendar.css';
 import './style/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom';
 import MyCalendar from './Components/Calendar';
-import Holiday from './Components/Holiday';
-import Event from './Components/Event';
-import Note from './Components/Note';
 import { Header } from './Components/Header';
 
 function App() {
 
   return (<>
-    <Router>
+    
       <Header />
-      <Switch>
+      <MyCalendar/>
+      {/* <Switch>
         <Route exact path='/' component={MyCalendar} />
         <Route exact path='/holiday' component={Holiday} />
         <Route exact path='/event' component={Event} />
         <Route exact path='/note' component={Note} />
-      </Switch>
-    </Router>
+      </Switch> */}
+    
   </>
   );
 }
