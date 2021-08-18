3fd9e8234f957534d98064ac4f2220c3607dcc5e
Elena Izotova <elm57650@gmail.com>
2021-08-18 10:46:48 +0700

    delete unnecessary data

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,8 @@ import { Header } from './Components/Header';
 function App() {
 
   return (<>
-    
       <Header />
       <MyCalendar/>
-      {/* <Switch>
-        <Route exact path='/' component={MyCalendar} />
-        <Route exact path='/holiday' component={Holiday} />
-        <Route exact path='/event' component={Event} />
-        <Route exact path='/note' component={Note} />
-      </Switch> */}
-    
   </>
   );
 }
